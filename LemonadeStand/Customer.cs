91a93708d9b89232476dd87e19f7d8bc92d3abd8
jcanderson195 <jcanderson195@gmail.com>
2016-03-30 17:09:16 -0500

    Reorganized customer class, anything that relates to the customer is in the customer class

--- LemonadeStand/Customer.cs
+++ LemonadeStand/Customer.cs
@@ -8,7 +8,9 @@ namespace LemonadeStand
 {
     public class Customer
     {
-        public int temperature;
+        
+        
+        public int potentialCustomerNumber;
 
         public Random rnd = new Random();
 
@@ -31,7 +33,7 @@ namespace LemonadeStand
         }
 
         
-        public void ThirstyCustomer()
+        public void ThirstyCustomer(int temperature)
         {
             
             if (temperature >= 50 && lemonadePrice <= .70)
@@ -46,7 +48,7 @@ namespace LemonadeStand
             }
         }
 
-        public void WeatherDependantCustomer()
+        public void WeatherDependantCustomer(int temperature)
         {
             if (temperature >= 75 && weatherCondition == Sunny)
             {
@@ -72,7 +74,7 @@ namespace LemonadeStand
             }
         }
 
-        public void CheapCustomer()
+        public void CheapCustomer(int temperature)
         {
             if (temperature >= 60 && weatherCondition == Sunny && lemonadePrice <= .25)
             {
@@ -85,6 +87,123 @@ namespace LemonadeStand
                 noLemonade.NoLemonadeBought();
             }
         }
+
+
+        public int PotentialCustomerGenerator(int temperature)
+        {
+            if (temperature <= 69)
+            {
+                potentialCustomerNumber = 100;
+                Console.WriteLine(potentialCustomerNumber + " potential customers");
+            }
+
+            else if (temperature <= 89)
+            {
+                potentialCustomerNumber = 150;
+                Console.WriteLine(potentialCustomerNumber + " potential customers");
+            }
+
+            else if (temperature <= 94)
+            {
+                potentialCustomerNumber = 175;
+                Console.WriteLine(potentialCustomerNumber + " potential customers");
+            }
+
+            else if (temperature >= 95)
+            {
+                potentialCustomerNumber = 200;
+                Console.WriteLine(potentialCustomerNumber + " potential customers");
+            }
+
+            int resultPotentialCustomerNumber = potentialCustomerNumber;
+            return resultPotentialCustomerNumber;
+        }
+
+
+        public void CustomersForPC100(int temperarture)
+        {
+            
+            if (potentialCustomerNumber == 100)
+            {
+                for (int thirstyCustomers = 0; thirstyCustomers < 30; thirstyCustomers++)
+                {
+                    ThirstyCustomer(temperarture);
+                }
+                for (int weatherDepeandantCustomers = 0; weatherDepeandantCustomers < 30; weatherDepeandantCustomers++)
+                {
+                    WeatherDependantCustomer(temperarture);
+                }
+                for (int cheapCustomers = 0; cheapCustomers < 40; cheapCustomers++)
+                {
+                    CheapCustomer(temperarture);
+                }
+            }
+        }
+
+        public void CustomersForPC150(int temperature)
+        {
+            
+
+            if (potentialCustomerNumber == 150)
+            {
+                
+                for (int thirstyCustomers = 0; thirstyCustomers < 50; thirstyCustomers++)
+                {
+
+                    ThirstyCustomer(temperature);
+                }
+                for (int weatherDepeandantCustomers = 0; weatherDepeandantCustomers < 50; weatherDepeandantCustomers++)
+                {
+                    WeatherDependantCustomer(temperature);
+                }
+                for (int cheapCustomers = 0; cheapCustomers < 50; cheapCustomers++)
+                {
+                    CheapCustomer(temperature);
+                }
+            }
+        }
+
+        public void CustomersForPC175(int temperature)
+        {
+           
+            if (potentialCustomerNumber == 175)
+            {
+                for (int thirstyCustomers = 0; thirstyCustomers < 75; thirstyCustomers++)
+                {
+                    ThirstyCustomer(temperature);
+                }
+                for (int weatherDepeandantCustomers = 0; weatherDepeandantCustomers < 50; weatherDepeandantCustomers++)
+                {
+                    WeatherDependantCustomer(temperature);
+                }
+                for (int cheapCustomers = 0; cheapCustomers < 50; cheapCustomers++)
+                {
+                    CheapCustomer(temperature);
+                }
+            }
+        }
+        public void CustomersForPC200(int temperature)
+        {
+            
+
+            if (potentialCustomerNumber == 200)
+            {
+                for (int thirstyCustomers = 0; thirstyCustomers < 80; thirstyCustomers++)
+                {
+                    ThirstyCustomer(temperature);
+                }
+                for (int weatherDepeandantCustomers = 0; weatherDepeandantCustomers < 60; weatherDepeandantCustomers++)
+                {
+                    WeatherDependantCustomer(temperature);
+                }
+                for (int cheapCustomers = 0; cheapCustomers < 60; cheapCustomers++)
+                {
+                    CheapCustomer(temperature);
+                }
+            }
+        }
+
+
 
 
     }
--- LemonadeStand/Weather.cs
+++ LemonadeStand/Weather.cs
@@ -13,11 +13,9 @@ namespace LemonadeStand
 
         public int potentialCustomerNumber;
         public int resultPotentialCustomerNumber;
-        
 
         
         
-
         public Weather()
         {
             
@@ -30,9 +28,9 @@ namespace LemonadeStand
 
         public void TemperatureGenerator()
         {
-
+            Customer newPotentialCustomers = new Customer();
             Console.WriteLine(temperature + " degrees");
-            PotentialCustomerGenerator();
+            newPotentialCustomers.PotentialCustomerGenerator(temperature);
         }
 
         public void WeatherConditionGenerator()
@@ -70,127 +68,7 @@ namespace LemonadeStand
 
         }
 
-        public int PotentialCustomerGenerator()
-        {
-            if (temperature <= 69)
-            {
-                potentialCustomerNumber = 100;
-                Console.WriteLine(potentialCustomerNumber + " potential customers");
-            }
-
-            else if (temperature <= 89)
-            {
-                potentialCustomerNumber = 150;
-                Console.WriteLine(potentialCustomerNumber + " potential customers");
-            }
-
-            else if (temperature <= 94)
-            {
-                potentialCustomerNumber = 175;
-                Console.WriteLine(potentialCustomerNumber + " potential customers");
-            }
-
-            else if (temperature >= 95)
-            {
-                potentialCustomerNumber = 200;
-                Console.WriteLine(potentialCustomerNumber + " potential customers");
-            }
-
-            int resultPotentialCustomerNumber = potentialCustomerNumber;
-            return resultPotentialCustomerNumber;
-        }
-
-
-        public void CustomersForPC100()
-        {
-            Customer newThirstyCustomer = new Customer();
-            Customer newWeatherCustomer = new Customer();
-            Customer newCheapCustomer = new Customer();
-
-            if (potentialCustomerNumber == 100)
-            {
-                for (int thirstyCustomers = 0; thirstyCustomers < 30; thirstyCustomers++)
-                {
-                    newThirstyCustomer.ThirstyCustomer();
-                }
-                for (int weatherDepeandantCustomers = 0; weatherDepeandantCustomers < 30; weatherDepeandantCustomers++)
-                {
-                    newWeatherCustomer.WeatherDependantCustomer();
-                }
-                for (int cheapCustomers = 0; cheapCustomers < 40; cheapCustomers++)
-                {
-                    newCheapCustomer.CheapCustomer();
-                }
-            }
-        }
-
-        public void CustomersForPC150()
-        {
-            Customer newThirstyCustomer = new Customer();
-            Customer newWeatherCustomer = new Customer();
-            Customer newCheapCustomer = new Customer();
-
-            if (potentialCustomerNumber == 150)
-            {
-                for (int thirstyCustomers = 0; thirstyCustomers < 50; thirstyCustomers++)
-                {
-                    newThirstyCustomer.ThirstyCustomer();
-                }
-                for (int weatherDepeandantCustomers = 0; weatherDepeandantCustomers < 50; weatherDepeandantCustomers++)
-                {
-                    newWeatherCustomer.WeatherDependantCustomer();
-                }
-                for (int cheapCustomers = 0; cheapCustomers < 50; cheapCustomers++)
-                {
-                    newCheapCustomer.CheapCustomer();
-                }
-            }
-        }
-
-        public void CustomersForPC175()
-        {
-            Customer newThirstyCustomer = new Customer();
-            Customer newWeatherCustomer = new Customer();
-            Customer newCheapCustomer = new Customer();
-
-            if (potentialCustomerNumber == 175)
-            {
-                for (int thirstyCustomers = 0; thirstyCustomers < 75; thirstyCustomers++)
-                {
-                    newThirstyCustomer.ThirstyCustomer();
-                }
-                for (int weatherDepeandantCustomers = 0; weatherDepeandantCustomers < 50; weatherDepeandantCustomers++)
-                {
-                    newWeatherCustomer.WeatherDependantCustomer();
-                }
-                for (int cheapCustomers = 0; cheapCustomers < 50; cheapCustomers++)
-                {
-                    newCheapCustomer.CheapCustomer();
-                }
-            }
-        }
-        public void CustomersForPC200()
-        {
-            Customer newThirstyCustomer = new Customer();
-            Customer newWeatherCustomer = new Customer();
-            Customer newCheapCustomer = new Customer();
-
-            if (potentialCustomerNumber == 200)
-            {
-                for (int thirstyCustomers = 0; thirstyCustomers < 80; thirstyCustomers++)
-                {
-                    newThirstyCustomer.ThirstyCustomer();
-                }
-                for (int weatherDepeandantCustomers = 0; weatherDepeandantCustomers < 60; weatherDepeandantCustomers++)
-                {
-                    newWeatherCustomer.WeatherDependantCustomer();
-                }
-                for (int cheapCustomers = 0; cheapCustomers < 60; cheapCustomers++)
-                {
-                    newCheapCustomer.CheapCustomer();
-                }
-            }
-        }
+        
     }
 }
 
